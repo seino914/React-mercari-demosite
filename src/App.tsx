5caf92a8397ef354e063328cfd19eaf7e9148ac4
seino914 <tonosaki914@icloud.com>
2024-06-24 01:35:48 +0900

    create categorie

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
+import { Categories } from './components';
 import { PageNavigation } from './layouts';
 
 function App() {
   return (
     <div>
+      <div className="sticky top-0 z-10 bg-white">
+        <Categories />
+      </div>
       <div className="min-h-screen"></div>
       <PageNavigation />
     </div>
